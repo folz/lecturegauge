4b9330bc4b7065c22ddafbcdc1026a7cef9f6b4d
Rodney Folz <rodney@rodneyfolz.com>
2014-11-16 05:46:22 -0500

    Reactify the comments list.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,9 @@
+/* global React */
+
 var CommentFeedbackPage = React.createClass({
     render: function() {
         return (
-        <div className="container">
-            <div className="row">
-                <div className="col col-xs-12 current-time">
-                    <p className="text-center">11:23 a.m.</p>
-                </div>
-            </div>
-
-            <div className="row">
-                <div className="col col-xs-12">
-                    <div className="progress">
-                        <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{"width": "60%"}}>
-                            <span className="sr-only">60% Complete</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
             <div className="row">
                 <div className="col col-xs-4">
                     <button type="button" className="btn btn-block btn-success understand-button">
@@ -51,12 +38,73 @@ var CommentFeedbackPage = React.createClass({
     }
 });
 
+var Comment = React.createClass({
+    render: function() {
+        var cx = React.addons.classSet;
+
+        var classes = cx({
+            "col": true,
+            "col-xs-1": true,
+            "green-block": this.props.commentType === 'SUCCESS',
+            "yellow-block": this.props.commentType === 'WARNING',
+            "red-block": this.props.commentType === 'FAILURE'
+        });
+
+        var srCommentText = cx({
+            "I understand.": this.props.commentType === 'SUCCESS',
+            "I'm a little confused.": this.props.commentType === 'WARNING',
+            "I don't understand.": this.props.commentType === 'FAILURE'
+        });
+
+        return (
+            <div className="row feedback-row">
+                <div className={classes}>
+                    <span className="sr-only">{srCommentText}</span>
+                </div>
+                <div className="col col-xs-9">
+                    <p>{this.props.text}</p>
+                    <p className="timestamp">{this.props.timestamp}</p>
+                </div>
+            </div>
+        );
+    }
+});
+
+var CommentList = React.createClass({
+    render: function() {
+        return (
+        <div>
+            <Comment commentType={'SUCCESS'} text={"This makes sense to me."} timestamp={"3:32"} />
+            <Comment commentType={'FAILURE'} text={"How do I initialize Firebase?"} timestamp={"3:41"} />
+            <Comment commentType={'WARNING'} text={"So I just make new CSS classes when I want to change things?"} timestamp={"4:01"} />
+            <Comment commentType={'WARNING'} text={"This makes sense to me."} timestamp={"4:17"} />
+        </div>
+        );
+    }
+});
+
 var App = React.createClass({
     render: function() {
         return (
-            <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-xs-12 current-time">
+                        <p className="text-center">11:23 a.m.</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col col-xs-12">
+                        <div className="progress">
+                            <div className="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{"width": "60%"}}>
+                                <span className="sr-only">60% Complete</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <CommentFeedbackPage />
-                <p>Hello, React!</p>
+                <CommentList />
             </div>
         );
     }
